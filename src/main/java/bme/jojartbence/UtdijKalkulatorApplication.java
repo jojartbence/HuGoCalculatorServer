50c7c41a3d50e37bd5cd8eed39fc69ce3b4189c4
jojartbence <jojart.bence96@gmail.com>
2020-05-09 12:43:28 +0200

    Base application class modified to use java fx

--- src/main/java/bme/jojartbence/UtdijKalkulatorApplication.java
+++ src/main/java/bme/jojartbence/UtdijKalkulatorApplication.java
@@ -3,12 +3,35 @@ package bme.jojartbence;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import javafx.application.*;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
 
 @SpringBootApplication
-public class UtdijKalkulatorApplication {
+public class UtdijKalkulatorApplication extends Application {
+	
+	private ConfigurableApplicationContext springContext;
+	private Parent rootNode;
 
-	public static void main(String[] args) {		
-		SpringApplication.run(UtdijKalkulatorApplication.class, args);
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+	
+	@Override
+	public void init() throws Exception {
+		springContext = SpringApplication.run(UtdijKalkulatorApplication.class);
 	}
 
+	@Override
+	public void start(Stage arg0) throws Exception {
+		
+	}
+
+	@Override
+	public void stop() throws Exception {
+		springContext.close();
+	}
 }
